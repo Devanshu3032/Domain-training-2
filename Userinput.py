0567469bff6107e5138cb596104265c6879cfae8
Devanshu3032 <devanshukumar841@gmail.com>
2025-08-19 12:03:07 +0530

    "dadwa

---
+++ Userinput.py
@@ -0,0 +1,14 @@
+a= input("Enter a name: ")
+res = "" 
+for i in a:
+    if i.isalpha(): 
+        res = res + i
+
+    res1 = res.capitalize()
+print(res1)    
+        
+        
+         Assignment Qes : 
+             
+         find number of word in paragraph (Input is string) 
+         write a prog to display digital clock
