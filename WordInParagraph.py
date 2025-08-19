19449b8526b49a1d5d3c03bf10c9bce898017c0e
Devanshu3032 <devanshukumar841@gmail.com>
2025-08-19 14:31:34 +0530

    "basic"

---
+++ WordInParagraph.py
@@ -0,0 +1,3 @@
+a = "diafjefsjf fef jsifjsj efjsif sf ssfies ejfijs fesjef"
+res = 0
+fo
---
+++ wordinpara.java
@@ -0,0 +1,9 @@
+class wordinpara{
+    public static void main(String[] args) {
+        String a = "danfsenfusefbsejbfesf  djfiijseifjsifdjkf fesfsd   esfsfeesf"  ;
+        int count = 0 ;
+        for (int i = 0; i < a.length(); i++) {
+            
+        } 
+    }
+}
